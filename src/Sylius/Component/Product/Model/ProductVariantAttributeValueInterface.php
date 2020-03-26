8de7aec48b70e3eca667cedbe6f36a044d89db22
Nikos Papagiannopoulos <npapagiannopoulos@printplanet.de>
2020-03-26 10:31:56 +0100

    Add ProductVariantAttributeValue & ORM

---
+++ src/Sylius/Component/Product/Model/ProductVariantAttributeValueInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the PrintPlanet/Sylius package.
+ *
+ * (c) Nikos Papagiannopoulos
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ *   This file is part of the Sylius package.
+ *
+ *   (c) Paweł Jędrzejewski
+ *
+ *   For the full copyright and license information, please view the LICENSE
+ *   file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PrintPlanet\Sylius\Component\Product\Model;
+
+use PrintPlanet\Sylius\Component\Attribute\Model\AttributeValueInterface as BaseAttributeValueInterface;
+
+interface ProductVariantAttributeValueInterface extends BaseAttributeValueInterface
+{
+    public function getProductVariant(): ?ProductVariantInterface;
+
+    public function setProductVariant(?ProductVariantInterface $productVariant): void;
+}
